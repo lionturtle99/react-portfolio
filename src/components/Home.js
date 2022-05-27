5cf8b4a71e9db11cfecd8ab755576c6eb1a0d488
Liam Eller <liamthelastson@gmail.com>
2022-05-26 18:00:18 -0700

    switch out project

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,23 +1,25 @@
 function Home() {
   return (
-    <div className="home-bg-img">
-      <section id="home" className="home">
-        <div className="container">
-          <div className="row">
-            <div className="col-md-1">
-            </div>
-            <div className="col-md-11">
-              <h1 className="display fw-bold"><span className="text-light">LIAM</span> <br></br>ELLER</h1>
-              <h3 className="text-danger">Software Engineer</h3>
-              <div className="buttons">
-                <a href="#contact-me" className="btn btn-danger mx-4 my-2">Contact me</a>
-                <a href="#about" className="btn btn-outline-danger mx-4 my-2">About</a>
+    <section>
+      <div className="home-bg-img">
+        <div className="home">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-1">
+              </div>
+              <div className="col-md-11">
+                <h1 className="display fw-bold"><span className="text-light">LIAM</span> <br></br>ELLER</h1>
+                <h3 className="text-danger">Software Engineer</h3>
+                <div className="buttons">
+                  <a href="#contact" className="btn btn-danger mx-4 my-2">Contact me</a>
+                  <a href="#about" className="btn btn-outline-danger mx-4 my-2">About</a>
+                </div>
               </div>
             </div>
           </div>
         </div>
-      </section>
-    </div>
+      </div>
+    </section>
   )
 }
 
